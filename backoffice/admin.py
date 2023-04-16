f589463a621a5428142b670fff88544ea33b4ad7
DelVMARE <bauwens.delphine@gmail.com>
2023-04-16 16:43:46 +0200

    models + django interface

--- backoffice/admin.py
+++ backoffice/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Editeur, Auteur, Genre, Livre, Stock, Usure, Exemplaire, Adherent, Emprunt
+
+admin.site.register(Editeur)
+admin.site.register(Auteur)
+admin.site.register(Genre)
+admin.site.register(Livre)
+admin.site.register(Stock)
+admin.site.register(Usure)
+admin.site.register(Exemplaire)
+admin.site.register(Adherent)
+admin.site.register(Emprunt)
--- backoffice/models.py
+++ backoffice/models.py
@@ -1,16 +1,26 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
 class Editeur(models.Model):
     nom = models.CharField(max_length=250)
+    def __str__(self):
+        return self.nom
 
 class Auteur(models.Model):
     nom = models.CharField(max_length=250)
     prenom = models.CharField(max_length=250)
+    def __str__(self):
+        return self.nom + " " + self.prenom
 
 class Genre(models.Model):
     nom = models.CharField(max_length=250)
+    def __str__(self):
+        return self.nom
 
 class Livre(models.Model):
     titre = models.CharField(max_length=250)
@@ -20,17 +30,25 @@ class Livre(models.Model):
     editeur = models.ForeignKey(Editeur, on_delete=models.CASCADE, default=None)
     auteur = models.ForeignKey(Auteur, on_delete=models.CASCADE, default=None)
     genres = models.ManyToManyField(Genre)
+    def __str__(self):
+        return self.titre
 
 class Stock(models.Model):
     emplacement = models.CharField(max_length=250)
+    def __str__(self):
+        return self.emplacement
 
 class Usure(models.Model):
     nom = models.CharField(max_length=250)
+    def __str__(self):
+        return self.nom
 
 class Exemplaire(models.Model):
     livre = models.ForeignKey(Livre, on_delete=models.CASCADE, default=None)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, default=None)
     usure = models.ForeignKey(Usure, on_delete=models.CASCADE, default=None)
+    def __str__(self):
+        return self.livre
 
 class Adherent(models.Model):
     nom = models.CharField(max_length=250)
@@ -38,6 +56,8 @@ class Adherent(models.Model):
     telephone = models.CharField(max_length=250)
     email = models.EmailField
     caution = models.BooleanField
+    def __str__(self):
+        return self.nom + " " + self.prenom
 
 class Emprunt(models.Model):
     adherent = models.ForeignKey(Adherent, on_delete=models.CASCADE, default=None)
@@ -45,3 +65,7 @@ class Emprunt(models.Model):
     date_emprunt = models.DateField
     date_retour = models.DateField
     status = models.CharField(max_length=250)
+    def __str__(self):
+        return self.date_emprunt
+    def emprunte_recemment(self):
+        return self.date_emprunt >= timezone.now() - datetime.timedelta(days=1)
